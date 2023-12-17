a5c483e9f1bdba81fe600c4481d2a5d7f7d765e0
quantumshiro <hiro.1nakanishi@gmail.com>
2023-12-18 04:10:47 +0900

    add: claculate distance in euclid space method

--- src/kmeans.zig
+++ src/kmeans.zig
@@ -15,9 +15,51 @@ const PATTERN = [PATT][DIMENTION]f16{
     [_]f16{ 7.0, -4.0 },
 };
 
-const CENTERPOSIT = [CLASS][DIMENTION]f16{
+const INITCENTERPOSIT = [CLASS][DIMENTION]f16{
     [_]f16{ 1.0, 1.0 },
     [_]f16{ 1.0, -1.0 },
     [_]f16{ -1.0, -1.0 },
     [_]f16{ -1.0, 1.0 },
 };
+
+pub fn distance(dim: i8, point1: *f16, point2: *f16) f16 {
+    var rntDistance: f16 = 0.0;
+    for (0..dim) |i| {
+        rntDistance += (point1[i] - point2[i]) * (point1[i] - point2[i]);
+    }
+    return std.math.sqrt2(rntDistance);
+}
+
+pub fn main() !void {
+    var endFlag: bool = false;
+    _ = endFlag;
+    var centerPosit: [CLASS][DIMENTION]f16 = undefined;
+    var sum: [CLASS][DIMENTION]f16 = undefined;
+    var elementNum: [CLASS]i8 = undefined;
+    var tempDisance: [CLASS]f16 = undefined;
+    var class_num: [CLASS]i8 = undefined;
+    _ = class_num;
+
+    for (0..DIMENTION) |i| {
+        for (0..DIMENTION) |j| {
+            centerPosit[i][j] = INITCENTERPOSIT[i][j];
+        }
+    }
+
+    while (1) {
+        // initialize
+        for (0..CLASS) |i| {
+            for (0..DIMENTION) |j| {
+                sum[i][j] = 0.0;
+            }
+            elementNum[i] = 0;
+        }
+
+        // decide class
+        for (0..PATT) |i| {
+            for (0..CLASS) |j| {
+                tempDisance[j] = distance(DIMENTION, &PATTERN[j][0], &centerPosit[i][0]);
+            }
+        }
+    }
+}
